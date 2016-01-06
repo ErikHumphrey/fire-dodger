4054ccdf94056cdcd9e13f808fb49fb4ea0ad469
Erik Humphrey <ContactErikH@gmail.com>
2016-01-06 11:49:50 -0500

    initial commit

---
+++ HumphreyErik23MovingBlocks/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace HumphreyErik23MovingBlocks
+{
+    partial class frmMovingBlocks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMovingBlocks));
+            this.picHero = new System.Windows.Forms.PictureBox();
+            this.picChips = new System.Windows.Forms.PictureBox();
+            this.tmrScore = new System.Windows.Forms.Timer(this.components);
+            this.tmrFires = new System.Windows.Forms.Timer(this.components);
+            this.picFire1 = new System.Windows.Forms.PictureBox();
+            this.picFire2 = new System.Windows.Forms.PictureBox();
+            this.picFire4 = new System.Windows.Forms.PictureBox();
+            this.picFire3 = new System.Windows.Forms.PictureBox();
+            this.picFire5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picChips)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHero
+            // 
+            this.picHero.BackColor = System.Drawing.Color.Transparent;
+            this.picHero.Image = ((System.Drawing.Image)(resources.GetObject("picHero.Image")));
+            this.picHero.InitialImage = ((System.Drawing.Image)(resources.GetObject("picHero.InitialImage")));
+            this.picHero.Location = new System.Drawing.Point(54, 262);
+            this.picHero.Name = "picHero";
+            this.picHero.Size = new System.Drawing.Size(52, 106);
+            this.picHero.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picHero.TabIndex = 0;
+            this.picHero.TabStop = false;
+            // 
+            // picChips
+            // 
+            this.picChips.BackColor = System.Drawing.Color.Transparent;
+            this.picChips.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.chips;
+            this.picChips.Location = new System.Drawing.Point(640, 52);
+            this.picChips.Name = "picChips";
+            this.picChips.Size = new System.Drawing.Size(76, 86);
+            this.picChips.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picChips.TabIndex = 1;
+            this.picChips.TabStop = false;
+            // 
+            // tmrScore
+            // 
+            this.tmrScore.Interval = 1000;
+            this.tmrScore.Tick += new System.EventHandler(this.tmrScore_Tick);
+            // 
+            // tmrFires
+            // 
+            this.tmrFires.Interval = 10;
+            this.tmrFires.Tick += new System.EventHandler(this.tmrFires_Tick);
+            // 
+            // picFire1
+            // 
+            this.picFire1.BackColor = System.Drawing.Color.Transparent;
+            this.picFire1.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.fireRed;
+            this.picFire1.Location = new System.Drawing.Point(293, 12);
+            this.picFire1.Name = "picFire1";
+            this.picFire1.Size = new System.Drawing.Size(58, 76);
+            this.picFire1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFire1.TabIndex = 2;
+            this.picFire1.TabStop = false;
+            // 
+            // picFire2
+            // 
+            this.picFire2.BackColor = System.Drawing.Color.Transparent;
+            this.picFire2.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.fireRed;
+            this.picFire2.Location = new System.Drawing.Point(213, 335);
+            this.picFire2.Name = "picFire2";
+            this.picFire2.Size = new System.Drawing.Size(58, 76);
+            this.picFire2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFire2.TabIndex = 3;
+            this.picFire2.TabStop = false;
+            // 
+            // picFire4
+            // 
+            this.picFire4.BackColor = System.Drawing.Color.Transparent;
+            this.picFire4.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.fireGreen;
+            this.picFire4.Location = new System.Drawing.Point(468, 106);
+            this.picFire4.Name = "picFire4";
+            this.picFire4.Size = new System.Drawing.Size(58, 76);
+            this.picFire4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFire4.TabIndex = 4;
+            this.picFire4.TabStop = false;
+            // 
+            // picFire3
+            // 
+            this.picFire3.BackColor = System.Drawing.Color.Transparent;
+            this.picFire3.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.fireBlue;
+            this.picFire3.Location = new System.Drawing.Point(370, 178);
+            this.picFire3.Name = "picFire3";
+            this.picFire3.Size = new System.Drawing.Size(58, 76);
+            this.picFire3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFire3.TabIndex = 5;
+            this.picFire3.TabStop = false;
+            // 
+            // picFire5
+            // 
+            this.picFire5.BackColor = System.Drawing.Color.Transparent;
+            this.picFire5.Image = global::HumphreyErik23MovingBlocks.Properties.Resources.fireGreen;
+            this.picFire5.Location = new System.Drawing.Point(468, 188);
+            this.picFire5.Name = "picFire5";
+            this.picFire5.Size = new System.Drawing.Size(58, 76);
+            this.picFire5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picFire5.TabIndex = 6;
+            this.picFire5.TabStop = false;
+            // 
+            // frmMovingBlocks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(804, 423);
+            this.Controls.Add(this.picFire5);
+            this.Controls.Add(this.picFire3);
+            this.Controls.Add(this.picFire4);
+            this.Controls.Add(this.picFire2);
+            this.Controls.Add(this.picFire1);
+            this.Controls.Add(this.picChips);
+            this.Controls.Add(this.picHero);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMovingBlocks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Erik Humphrey\'s Quest for Chips";
+            this.Load += new System.EventHandler(this.frmMovingBlocks_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMovingBlocks_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMovingBlocks_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picHero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picChips)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFire5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHero;
+        private System.Windows.Forms.PictureBox picChips;
+        private System.Windows.Forms.Timer tmrScore;
+        private System.Windows.Forms.Timer tmrFires;
+        private System.Windows.Forms.PictureBox picFire1;
+        private System.Windows.Forms.PictureBox picFire2;
+        private System.Windows.Forms.PictureBox picFire4;
+        private System.Windows.Forms.PictureBox picFire3;
+        private System.Windows.Forms.PictureBox picFire5;
+    }
+}
+
---
+++ HumphreyErik23MovingBlocks/Form1.cs
@@ -0,0 +1,221 @@
+﻿/*
+___  ___           _              ______ _            _        
+|  \/  |          (_)             | ___ \ |          | |       
+| .  . | _____   ___ _ __   __ _  | |_/ / | ___   ___| | _____ 
+| |\/| |/ _ \ \ / / | '_ \ / _` | | ___ \ |/ _ \ / __| |/ / __|
+| |  | | (_) \ V /| | | | | (_| | | |_/ / | (_) | (__|   <\__ \
+\_|  |_/\___/ \_/ |_|_| |_|\__, | \____/|_|\___/ \___|_|\_\___/
+                            __/ |                              
+                           |___/                                   */                 
+
+/* Author: Erik Humphrey
+ * Project title: Project 23 - Moving Blocks
+ * Date started: December 14th
+ * Executable name: HumphreyErik23MovingBlocks.exe
+ * Description: Game with moving image character controlled by user. User wins or loses based on objects they collide with; some move.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumphreyErik23MovingBlocks
+{
+    public partial class frmMovingBlocks : Form
+    {
+        // Global declarations
+        bool sRight, sLeft, sUp, sDown;
+        int[] yFVelocity = new int[5];
+        int scoreTimer;
+
+        public frmMovingBlocks()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMovingBlocks_Load(object sender, EventArgs e)
+        {
+            // Set speed of fires
+            yFVelocity[0] = 1;
+            yFVelocity[1] = 2;
+            yFVelocity[2] = 3;
+            yFVelocity[3] = 3;
+            yFVelocity[4] = -3;
+            // Show tutorial message, start game when it is closed
+            MessageBox.Show("Navigate around the walls of fire to reach the bag of chips!\r\n\r\nArrow keys to move.", "Tutorial");
+            tmrScore.Start();
+            tmrFires.Start();
+        }
+
+        // Stops pressing arrow keys
+        private void frmMovingBlocks_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    sRight = false;
+                    break;
+                case Keys.Left:
+                    sLeft = false;
+                    break;
+                case Keys.Up:
+                    sUp = false;
+                    break;
+                case Keys.Down:
+                    sDown = false;
+                    break;
+            }
+
+        }
+
+        // Starts pressing arrow keys
+        private void frmMovingBlocks_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    sRight = true;
+                    picHero.Image = Properties.Resources.hero;
+                    break;
+                case Keys.Left:
+                    sLeft = true;
+                    picHero.Image = Properties.Resources.heroleft;
+                    break;
+                case Keys.Up:
+                    sUp = true;
+                    break;
+                case Keys.Down:
+                    sDown = true;
+                    break;
+            }
+        }
+
+        private void tmrFires_Tick(object sender, EventArgs e)
+        {
+            // Declare moving fire picturebox array
+            PictureBox[] picFires = new PictureBox[] {
+				picFire1, picFire2, picFire3, picFire4, picFire5
+			};
+            int[] yFire = new int[5];
+
+            // Move hero based on keys pressed
+            if (sRight == true && picHero.Left + picHero.Width < this.ClientRectangle.Width)
+            {
+                picHero.Left += 3;
+            }
+            else if (sLeft == true && picHero.Left > this.ClientRectangle.Left)
+            {
+                picHero.Left -= 3;
+            }
+            else if (sUp == true && picHero.Top > this.ClientRectangle.Top)
+            {
+                picHero.Top -= 3;
+            }
+            else if (sDown == true && picHero.Top + picHero.Height < this.ClientRectangle.Height)
+            {
+                picHero.Top += 3;
+            }
+
+            // Moving fires
+            for (int i = 0; i < 5; i++)
+            {
+                yFire[i] = picFires[i].Top;
+                yFire[i] = yFire[i] + yFVelocity[i];
+            }
+
+            // Change direction
+            for (int i = 0; i < 5; i++)
+            {
+                if (yFire[i] < 0 || yFire[i] > this.ClientRectangle.Height - picFires[i].Height)
+                {
+                    yFVelocity[i] = (-1) * yFVelocity[i];
+                }
+
+                picFires[i].Top = yFire[i];
+            }
+
+            // Detect Hero collision with fires
+            for (int i = 0; i < 5; i++)
+            {
+                if (picHero.Bounds.IntersectsWith(picFires[i].Bounds))
+                {
+                    // Change image of hero to look dead (different depending on the direction he was facing)
+                    if (sRight) picHero.Image = Properties.Resources.right4dead;
+                    else picHero.Image = Properties.Resources.left4dead;
+                    endGame();
+                    DialogResult playAgain = MessageBox.Show(
+                        "You ran into a fire after " + scoreTimer + " seconds.\r\n\r\nTIP: Fires have square hitboxes, stay as far away from them as you can!",
+                        "Game over",
+                        // "Makes sure" the default focused button is Retry, but because the user is hitting the right arrow key, it will probably move to Cancel anyway
+                        MessageBoxButtons.RetryCancel, MessageBoxIcon.None, MessageBoxDefaultButton.Button1);
+                    if (playAgain == DialogResult.Retry)
+                    {
+                        resetGame();
+                    }
+                    else if (playAgain == DialogResult.Cancel)
+                    {
+                        this.Close(); // Close the program
+                    }
+                }
+            }
+
+            // Hero collision with bag of chips objective
+            if (picHero.Bounds.IntersectsWith(picChips.Bounds))
+            {
+                endGame();
+                DialogResult playAgain = MessageBox.Show("You reached the chips in " + scoreTimer + " seconds.\r\n\r\nWould you like to try for a better time?",
+                    "Victory!",
+                    MessageBoxButtons.RetryCancel, MessageBoxIcon.None, MessageBoxDefaultButton.Button1);
+                if (playAgain == DialogResult.Retry)
+                {
+                    resetGame();
+                }
+                else if (playAgain == DialogResult.Cancel)
+                {
+                    this.Close();
+                }
+            }
+
+        }
+
+        // Score timer that counts up 1/second and updates form title
+        private void tmrScore_Tick(object sender, EventArgs e)
+        {
+            scoreTimer += 1;
+            this.Text = "Erik Humphrey's Quest for Chips - Timer: " + scoreTimer;
+        }
+
+        void endGame()
+        {
+            // Stop timers
+            tmrFires.Stop();
+            tmrScore.Stop();
+        }
+
+        void resetGame()
+        {
+            sRight = sLeft = sDown = sUp = false;
+            // Unkill the player character
+            picHero.Image = Properties.Resources.hero;
+            // Return player character and fires to starting positions
+            picHero.Location = new Point(69, 197);
+            picFire1.Location = new Point(293, 12);
+            picFire2.Location = new Point(213, 335);
+            picFire3.Location = new Point(370, 178);
+            picFire4.Location = new Point(468, 106);
+            picFire5.Location = new Point(468, 188);
+            // Count score
+            scoreTimer = 0;
+            this.Text = "Erik Humphrey's Quest for Chips - Timer: " + scoreTimer;
+            tmrScore.Start();
+            // Allow the player and fires to move
+            tmrFires.Start();
+        }
+    }
+}
